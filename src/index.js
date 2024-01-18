8895bd884b5892e97731ebc622ad8ffb97103409
Gonzalo Lazarte <gonzalaz@live.com.ar>
2024-01-18 00:00:02 -0300

    Actualización de la aplicación

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 import ListaEspera from './ListaEspera'
 import CambiosListaEspera from "./CambiosListaEspera"
